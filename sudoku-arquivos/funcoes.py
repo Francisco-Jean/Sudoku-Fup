b1a0c3af4984edc359a9696dfc79be46ad655847
levy-oliveira <103868542+levy-oliveira@users.noreply.github.com>
2022-07-04 11:18:33 -0300

    Update funcoes.py

--- sudoku-arquivos/funcoes.py
+++ sudoku-arquivos/funcoes.py
@@ -58,10 +58,20 @@ def matriz(lin, col, val):
   m = [[val] * col for _ in range(lin)]
   return m
 
+
 def verificador(col,lin,valor):
   
+  #TRANSDORMAR LETRA EM NUMERO
+  let=['A', 'B', 'C' ,'D', 'E', 'F', 'G', 'H', 'I']
+  col = col.upper()
+  for i in range(25):
+    if col == let[i]:
+      col = i+1
+
+
 #VERIFICAR OS QUADRADOS 3X3
   passe = False
+
   if lin-1 <= 2 and col-1 <=2:
     for i in range(3):
       for j in range(3):
@@ -126,53 +136,54 @@ def verificador(col,lin,valor):
           passe = True
           break
 
-  if lin-1 < 0 or col-1 < 0 or valor <= 0 or lin-1 > 9 or col-1 > 9 or valor > 9:
-    
-    print("Jogada impossibilitada")
-    passe = True
  
   while passe == False:
+    
 
-    a[lin-1][col-1] = valor
+    aux = True
     
   #VERIFICAR AS LINHAS
     for j in range(9):
-      if a[lin-1][col-1] == a[lin-1][j] and j!= col-1 and a[lin-1][col-1] > 0:
-        a[lin-1][col-1] = " "
+      if valor == a[lin-1][j] and j!= col-1 and valor > 0:
         print("Jogada impossibilitada pela linha")
-        print("Tente novamente")
+        print("Tente novamente um valor diferente de %d"%valor)
+        aux = False
         break
+        
+
   #VERIFICAR AS COLUNAS
+    if aux == True:
+      for i in range(9):
+        if valor == a[i][col-1] and i != lin-1 and valor > 0:
+          print("Jogada impossibilitada pela coluna")
+          print("Tente novamente")
+          aux = False
+          break
 
-    for i in range(9):
-      if a[lin-1][col-1] == a[i][col-1] and i != lin-1 and a[lin-1][col-1] > 0:
-        a[lin-1][col-1] = ""
-        print("Jogada impossibilitada pela coluna")
-        print("Tente novamente")
-        break
+    if aux:
+      a[lin-1][col-1] =  valor
+        
+
     passe = True
-
-a = matriz(9,9,"")
-repetidor = True
-while repetidor:
-  x = input()
-  v = x.replace(":",",")
-  v = list(v.split(","))
-  v[0] = int(v[0])
-  v[1] = int(v[1])
-  v[2] = int(v[2])
-  v = verificador(v[0],v[1],v[2])
-
-  for i in range(9):
-    for j in range(9):
-      print(f"[{a[i][j]:^3}]", end="")
-    print("")
 
 # 05 - Função que verifica se uma jogada coincide com uma pista:
 
 
-# 06 - Função que verifica se uma jogada é válida:
 
+# 06 - Função que verifica se uma jogada é válida:
+def validar(jogada):
+  col=['A', 'B', 'C' ,'D', 'E', 'F', 'G', 'H', 'I']
+  #transformando as celulas da lista nos seus respectivos tipos corretos
+  j=jogada[0].upper()
+  try:
+    i=int(jogada[2])
+    k=int(jogada[4])
+  except:
+    return False
+    #condições pra validar a jogada
+  if type(i)!=int or type(k)!=int or j not in col or i>9 or i<1 or k>9 or k<1 :
+      print('Jogada Inválida.Entre um novo valor.')
+      return False
 
 # 07 - Função que deleta uma jogada requerida pelo jogador:
 
